fccc0b2e94e30bc684d84d2a6af18374d013af28
SamirM-dev <memmedovsamirws@gmail.com>
2026-08-10 18:34:17 +0400

    first commit

---
+++ src/main/java/com/example/RestSecurityTaskManagementSystem/RestSecurityTaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestSecurityTaskManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecurityTaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSecurityTaskManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestSecurityTaskManagementSystem/RestSecurityTaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestSecurityTaskManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSecurityTaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
