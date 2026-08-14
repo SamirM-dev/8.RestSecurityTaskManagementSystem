e4998998d72860156c91aab0078d6887084ef476
SamirM-dev <memmedovsamirws@gmail.com>
2026-08-14 10:18:50 +0400

    Filter and Interceptor

---
+++ src/main/java/com/example/RestSecurityTaskManagementSystem/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package com.example.RestSecurityTaskManagementSystem.config;
+
+import com.example.RestSecurityTaskManagementSystem.appfilter.LoggingFilter;
+import com.example.RestSecurityTaskManagementSystem.appfilter.RequestIdFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> loggingFilter(){
+        FilterRegistrationBean<LoggingFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new LoggingFilter());
+        registration.setOrder(1);
+        registration.addUrlPatterns("/api/*");
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestIdFilter> requestIdFilter(){
+        FilterRegistrationBean<RequestIdFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new RequestIdFilter());
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registration.addUrlPatterns("/api/*");
+        return registration;
+     }
+}
---
+++ src/main/java/com/example/RestSecurityTaskManagementSystem/interceptor/LogInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.RestSecurityTaskManagementSystem.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(handler instanceof HandlerMethod handlerMethod){
+            String controller = handlerMethod.getBeanType().getSimpleName();
+            String method = handlerMethod.getMethod().getName();
+            log.info("Handler : {}.{}",controller,method);
+        }
+        return true;
+    }
+
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        if (ex!=null){
+            log.error("Exception: {}",ex.getMessage());
+        }
+    }
+}
